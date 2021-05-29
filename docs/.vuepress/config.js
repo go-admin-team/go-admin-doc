4f8c94a0d2bf50bb4444022202319bc29eeb900b
zhangwenjian <zwj777@live.com>
2021-05-29 19:53:22 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,19 @@ module.exports = {
         rel: 'icon',
         href: '/favicon.ico'
       }
+    ],
+    [
+      'script',
+      {},
+      `
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?f98a2f382011d17906899de9f676b294";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+        `
     ]
   ],
   themeConfig: {
